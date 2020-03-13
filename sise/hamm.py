573f1d38c8c68077f4bd74c4149eb143c268e968
JKWrobel <bociek428@gmail.com>
2020-03-13 23:24:19 +0100

    Added Hamming heuristic

---
+++ sise/hamm.py
@@ -0,0 +1,14 @@
+from .node import Node
+
+
+def calc_hamm_dist(node):
+    dist = 0
+    height = len(node.data)
+    if height == 0:
+        return None
+    width = len(node.data[0])
+    for i in range(0, width):
+        for j in range(0, height):
+            if node.data[i][j] != i*4 + j:
+                dist += 1
+    return dist
