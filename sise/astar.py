06f45ce14191a9dd00da42fdea8d5c125ec5b36d
JKWrobel <bociek428@gmail.com>
2020-03-14 01:49:02 +0100

    Added astar algorythm

---
+++ sise/astar.py
@@ -0,0 +1,28 @@
+import heapq
+from sise import node
+
+
+def solve_puzzle_astar(root_node, heuristic):
+    priority_queue = []
+    depth = 0
+    heapq.heappush(priority_queue, (heuristic(root_node), 0, root_node))
+
+    def astar_step(current_node):
+        if heuristic(current_node) == 0:
+            return current_node
+        for direction in ['D', 'L', 'U', 'R']:
+            new_data = node.move(direction, current_node.data)
+
+            if new_data is not None and node.is_opposite_direction(direction, current_node.direction) is False:
+                child_node = node.Node(new_data, current_node, direction)
+                heapq.heappush(priority_queue, (heuristic(child_node), depth + 1, child_node))
+        return None
+
+    while True:
+        heuristic_value, depth, next_node = heapq.heappop(priority_queue)
+        solution_node = astar_step(next_node)
+
+        if solution_node is not None:
+            return solution_node
+
+
