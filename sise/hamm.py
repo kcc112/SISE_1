7a21cbc1079ce82f54640b614c5481f63d196a18
216929 Jakub Wróbel <216929@edu.p.lodz.pl>
2020-03-20 16:25:18 +0100

    Fixed out of bound error in hamm.py

--- sise/hamm.py
+++ sise/hamm.py
@@ -7,8 +7,8 @@ def calc_hamm_dist(node):
     if height == 0:
         return None
     width = len(node.data[0])
-    for i in range(0, width):
-        for j in range(0, height):
+    for i in range(0, height):
+        for j in range(0, width):
             if node.data[i][j] != i*width + j:
                 dist += 1
     return dist
