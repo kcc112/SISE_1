3f2a80acb0abf42112c1be86abd69d6a7ebf93ca
216929 Jakub Wróbel <216929@edu.p.lodz.pl>
2020-03-20 16:24:56 +0100

    Fixed bug in manh
    One bug made it give wrong distance values and other was out of bounds exception

--- sise/manh.py
+++ sise/manh.py
@@ -4,9 +4,9 @@ def calc_manh_dist(node):
     if height == 0:
         return None
     width = len(node.data[0])
-    for i in range(0, width):
-        for j in range(0, height):
-            correct_i = node.data[i][j] // height
+    for i in range(0, height):
+        for j in range(0, width):
+            correct_i = node.data[i][j] // width
             correct_j = node.data[i][j] % width
             dist += abs(correct_i - i)
             dist += abs(correct_j - j)
